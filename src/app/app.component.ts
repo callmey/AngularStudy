ce2fa09730da7a12243b733a50e5f0d99c55910b
dayoni <callmey@nate.com>
2018-05-15 01:43:54 +0900

    학생 수정 기능 추가

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,10 +9,15 @@ import { Student } from './Student';
 export class AppComponent {
   title = 'app works!';
   list = ['안녕하세요', 'hello'];
-  
+  selectedStudent: Student;
+
   students: Student[] = [
     new Student(1, '201132051', '홍길동', 'hgd@skhu.ac.kr'),
     new Student(2, '201132052', '임꺽정', 'lkj@skhu.ac.kr'),
     new Student(3, '201132053', '전우치', 'jwc@skhu.ac.kr'),
   ];
+
+  selectStudent(student: Student) {
+    this.selectedStudent = student;
+  }
 }
